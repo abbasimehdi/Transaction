e52d6c45d821d72029c82d90c483dfc00dae3b14
Abbasi Mehdi <abbasimhdi@gmail.com>
2024-01-01 18:00:54 +0330

    Refactor codes

--- SelfOfficeName/Modules/Domain/Transaction/src/Models/Transaction.php
+++ SelfOfficeName/Modules/Domain/Transaction/src/Models/Transaction.php
@@ -5,6 +5,8 @@ namespace Selfofficename\Modules\Domain\Transaction\Models;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Selfofficename\Modules\Domain\Card\Models\Card;
 use Selfofficename\Modules\Domain\Commission\Models\Commission;
 use Selfofficename\Modules\Domain\Transaction\database\factories\TransactionFactory;
@@ -15,16 +17,25 @@ class Transaction extends Model
 
     protected $fillable = ['source_card_id', 'destination_card_number', 'amount', 'status'];
 
+    /**
+     * @return Factory
+     */
     protected static function newFactory(): Factory
     {
         return TransactionFactory::new();
     }
 
+    /**
+     * @return BelongsTo
+     */
     public function card()
     {
         return $this->belongsTo(Card::class, 'source_card_id', 'id');
     }
 
+    /**
+     * @return HasOne
+     */
     public function commission()
     {
         return $this->hasOne(Commission::class);
